55ed2f288b68b24b5cba8c733d982e7d487d6a6f
Nickraph <nikosmozas@gmail.com>
2020-01-15 18:39:17 +0200

    update

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ io.sockets.on('connection', function(socket){//SOCKETS++++++
 
             if(players == 2){
                 var unames = player_accounts[0].username+"-VS-"+player_accounts[1].username;
-                io.broadcast("displayUsernames", {unames});
+                io.emit("displayUsernames", {unames});
             }
         }
         else{
